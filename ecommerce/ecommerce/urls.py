52c25cb494a21edf626a58d0d46a0cca9c724971
sanjana177402 <sanjanasonu2005@gmail.com>
2026-06-18 22:09:20 +0530

    fix: add frontend template routes and fix URL conflicts

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -3,6 +3,7 @@ from django.urls import path , include
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -17,6 +18,13 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('categories/', include('categories.urls')),
     path('orders/', include('orders.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('register/', TemplateView.as_view(template_name='register.html')),
+    path('login/', TemplateView.as_view(template_name='login.html')),
+    path('cart/', TemplateView.as_view(template_name='cart.html')),
+    path('orders/', TemplateView.as_view(template_name='orders.html')),
+    path('payment/', TemplateView.as_view(template_name='payment.html')),
+    path('wishlist/', TemplateView.as_view(template_name='wishlist.html')),
 ]
 
 
