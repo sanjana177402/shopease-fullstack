b9cf675b80ef93e1ae95e162ce55262dc4216520
sanjana177402 <sanjanasonu2005@gmail.com>
2026-06-20 22:44:45 +0530

    fix: register Order model in admin with status management

--- ecommerce/orders/admin.py
+++ ecommerce/orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'totalAmount', 'status', 'createdAt']
+    list_filter = ['status']
+    list_editable = ['status']
+    inlines = [OrderItemInline]
