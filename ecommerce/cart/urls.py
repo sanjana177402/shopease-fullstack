270872037a91360a97cc138e3d3464c11b78c357
sanjana177402 <sanjanasonu2005@gmail.com>
2026-06-20 21:31:41 +0530

    fix: force redeploy with cart routes

--- ecommerce/cart/urls.py
+++ ecommerce/cart/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from .views import CartView, AddToCart, UpdateCart, RemoveFromCart
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', AddToCart.as_view()),           
     path('update/', UpdateCart.as_view()),       
     path('remove/<int:productId>/', RemoveFromCart.as_view()), 
+    
 ]
